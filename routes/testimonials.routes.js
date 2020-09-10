518fc70a57ada3eff067e77fee4e26c52719779f
Michał Kopacz <kopaczmichal093@gmail.com>
2020-09-10 19:39:24 +0200

    Export DB

---
+++ routes/testimonials.routes.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+const db = require('./../db');
+
+router.route('/testimonials').get((req, res) => {
+    res.json(db.testimonials);
+});
+
+router.route('/testimonials:id').get((req, res) => {
+    res.json(db.testimonials.filter(item => item.id === parseInt(req.params.id)));
+});
+
+router.route('/testimonials/random').get((req, res) => {
+    const randomEl = db.testimonials[Math.floor(Math.random() * db.testimonials.length)];
+    res.json(db.testimonials.filter(item => item.id === randomEl.id));
+});
+
+router.route('/testimonials').post((req, res) => {
+    const endId = db[db.testimonials.length - 1].id + 1;
+    db.testimonials.push({
+        id: endId,
+        author: req.body.author,
+        text: req.body.text,
+    });
+    res.json({ message: 'OK'});
+});
+
+router.route('/testimonials:id').put((req, res) => {
+    db.testimonials.filter(item => item.id === parseInt(req.params.id)).forEach(item => {
+        item.author = req.body.author;
+        item.text = req.body.text;
+    });
+    res.json({ message: 'OK'});
+});
+
+router.route('/testimonials/:id').delete((req, res) => {
+    if(db.testimonials.indexOf(db.testimonials.filter(item => item.id === parseInt(req.params.id))[0]) > - 1)
+        db.testimonials.splice(db.testimonials.indexOf(db.testimonials.filter(item => item.id === parseInt(req.params.id))[0]), 1);
+    res.json({ message: 'OK'});
+});
--- server.js
+++ server.js
@@ -1,26 +1,21 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const db = require('./db');
 
 const app = express();
 
-const db = [
-    { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
-    { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
-    { id: 3, author: 'John Doe', text: 'This company is worth every coin!' },
-    { id: 4, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
-    { id: 5, author: 'John Doe', text: 'This company is worth every coin!' },
-    { id: 6, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
-    { id: 7, author: 'John Doe', text: 'This company is worth every coin!' },
-    { id: 8, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
-    { id: 9, author: 'John Doe', text: 'This company is worth every coin!' },
-    { id: 10, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
-];
+const testimonialRoute = require('./routes/testimonials.routes');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
+app.use('/', testimonialRoute);
+
+app.use((req, res) => {
+    res.status(404).json({message: 'Error 404 Page not found...'})
+});
 
 app.listen(8000, () => {
     console.log('Server is running on port: 8000');
