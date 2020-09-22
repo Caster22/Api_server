1acea60e92629cedf2bd2b5e5c3c9ddcd43851b0
Michał Kopacz <kopaczmichal093@gmail.com>
2020-09-22 14:24:22 +0200

    Modify testimonials endpoints

--- routes/testimonials.routes.js
+++ routes/testimonials.routes.js
@@ -1,42 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const db = require('./../db');
+const TestimonialController = require('../controllesrs/testimonials.controller');
 
-router.route('/testimonials').get((req, res) => {
-    res.json(db.testimonials);
-});
+router.get('/testimonials', TestimonialController.getAll);
 
-router.route('/testimonials/:id').get((req, res) => {
-    res.json(db.testimonials.filter(item => item.id === parseInt(req.params.id)));
-});
+router.get('/testimonials/:id', TestimonialController.getById);
 
-router.route('/testimonials/random').get((req, res) => {
-    const randomEl = db.testimonials[Math.floor(Math.random() * db.testimonials.length)];
-    res.json(db.testimonials.filter(item => item.id === parseInt(req.params.id)));
-});
+router.get('/testimonials/random', TestimonialController.getRandom);
 
-router.route('/testimonials').post((req, res) => {
-    const endId = db.testimonials[db.testimonials.length - 1].id + 1;
-    db.testimonials.push({
-        id: endId,
-        author: req.body.author,
-        text: req.body.text,
-    });
-    res.json({ message: 'OK'});
-});
+router.post('/testimonials', TestimonialController.postNew);
 
-router.route('/testimonials/:id').put((req, res) => {
-    db.testimonials.filter(item => item.id === parseInt(req.params.id)).forEach(item => {
-        item.author = req.body.author;
-        item.text = req.body.text;
-    });
-    res.json({ message: 'OK'});
-});
+router.put('/testimonials/:id', TestimonialController.updateById);
 
-router.route('/testimonials/:id').delete((req, res) => {
-    if(db.testimonials.indexOf(db.testimonials.filter(item => item.id === parseInt(req.params.id))[0]) > - 1)
-        db.testimonials.splice(db.testimonials.indexOf(db.testimonials.filter(item => item.id === parseInt(req.params.id))[0]), 1);
-    res.json({ message: 'OK'});
-});
+router.delete('/testimonials/:id', TestimonialController.removeById);
 
 module.exports = router;
