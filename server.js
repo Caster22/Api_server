152958ae5c5a8561a5a62b7a73a908504874af79
Michał Kopacz <kopaczmichal093@gmail.com>
2020-09-22 13:27:44 +0200

    Add use of mongodb and schema models

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const socket = require('socket.io');
+const mongoose = require('mongoose');
 
-const app = express();
+/* Import and use Endpoints */
 
 const testimonialsRoute = require('./routes/testimonials.routes');
 const concertsRoute = require('./routes/concerts.routes');
 const seatsRoute = require('./routes/seats.routes');
+
+const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -15,14 +18,14 @@ app.use(cors());
 
 app.use(express.static(path.join(__dirname, '/client/build')))
 
+app.use('/api', testimonialsRoute);
+app.use('/api', concertsRoute);
+app.use('/api', seatsRoute);
+
 app.use((req, res, next) => {
    req.io = io;
    next();
 });
-
-app.use('/api', testimonialsRoute);
-app.use('/api', concertsRoute);
-app.use('/api', seatsRoute);
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/client/build/index.html'));
@@ -32,9 +35,24 @@ app.use((req, res) => {
     res.status(404).json({message: 'Error 404 Page not found...'})
 });
 
+/* Start DataBase */
+
+mongoose.connect('mongodb://localhost:27017/NewWaveDB', { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('Connected to the database');
+});
+
+db.on('error', err => console.log('Error ' + err));
+
+/* Start Server */
+
 const server = app.listen(process.env.PORT || 8000, () => {
     console.log('Server is running on port: 8000');
 });
+
+/* Create use of socket */
 
 const io = socket(server);
 
